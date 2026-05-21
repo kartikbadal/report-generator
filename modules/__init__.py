6da4b2fc67f21ba00583f96b9d5d23bffc22246a
Kartik Badal <kartikbadal@gmail.com>
2026-05-21 10:06:26 +0530

    Fix import issue for Streamlit Cloud deployment

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,13 @@
-from .data_processor import load_file, clean_data, get_summary
-from .chart_generator import save_bar_chart, save_line_chart, save_correlation_heatmap
-from .pdf_generator import generate_pdf
-from .email_sender import send_report
-from .scraper import get_usd_to_inr, get_page_text
+# modules/__init__.py
+try:
+    from .data_processor import load_file, clean_data, get_summary
+    from .chart_generator import save_bar_chart, save_line_chart, save_correlation_heatmap
+    from .pdf_generator import generate_pdf
+    from .email_sender import send_report
+    from .scraper import get_usd_to_inr, get_page_text
+except ImportError:
+    from modules.data_processor import load_file, clean_data, get_summary
+    from modules.chart_generator import save_bar_chart, save_line_chart, save_correlation_heatmap
+    from modules.pdf_generator import generate_pdf
+    from modules.email_sender import send_report
+    from modules.scraper import get_usd_to_inr, get_page_text
